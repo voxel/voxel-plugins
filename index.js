858ff447887c8919b8a26aad3f577713b656082b
deathcap <deathcap@gmx.co.uk>
2014-01-28 20:37:15 -0800

    Test loading voxel-cs first instead of voxel-engine, removing game instance

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function Plugins(game, opts) {
 
   opts = opts || {};
   this.require = opts.require || require;
-  this.masterPluginName = opts.masterPluginName || 'voxel-engine'; // synthetic 'plugin' created as parent of all
+  this.masterPluginName = opts.masterPluginName || 'voxel-cs'; // synthetic 'plugin' created as parent of all
 
   // map plugin name to instances
   this.all = {};
@@ -60,13 +60,10 @@ Plugins.prototype.scanAndInstantiate = function(name, opts) {
 Plugins.prototype.instantiate = function(createPlugin, name, opts) {
   var plugin;
   if (!this.game && name === this.masterPluginName) {
-    // the 'master' plugin is the game object itself
-    this.game = plugin = createPlugin(opts);
+    // the 'master' plugin provides the game object itself
+    plugin = createPlugin(opts);
+    this.game = {}; // TODO: provide link to voxel-engine.. but client/server?
     this.game.plugins = this;
-    if (process.browser && this.game.notCapable()) {
-      if (window.document) window.document.body.appendChild(this.game.notCapableMessage()); // TODO: find out why notCapable() isn't showing up
-      throw new Error('[voxel-plugins] fatal error: your system is not capable of running voxel-engine (game.notCapable)');
-    }
   } else {
     plugin = createPlugin(this.game, opts); // requires (game, opts) convention
     if (!plugin) {
